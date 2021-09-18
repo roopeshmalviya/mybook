575518cf6419bc6bda98e11c02259b2b6b3bb147
unknown <roopeshmalviya7@gmail.com>
2021-09-18 18:59:28 +0530

    third Commit

--- src/main/java/com/mybook/controller/UserController.java
+++ src/main/java/com/mybook/controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.mybook.entity.User;
 
 @RestController
-@RequestMapping("/getUser")
+@RequestMapping("/user")
 public class UserController {
 	
 	@GetMapping
