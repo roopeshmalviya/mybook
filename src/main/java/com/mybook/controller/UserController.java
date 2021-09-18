e1e235c54fb2e7f03f6d2315067748df97d5eb41
unknown <roopeshmalviya7@gmail.com>
2021-09-18 18:50:23 +0530

    First Commit

---
+++ src/main/java/com/mybook/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.mybook.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mybook.entity.User;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	
+	@GetMapping
+	public Object getUser() {
+		return "Getting User";
+	}
+}
---
+++ src/main/java/com/mybook/entity/User.java
@@ -0,0 +1,38 @@
+package com.mybook.entity;
+
+public class User {
+	
+	private Integer userId;
+	
+	private String name;
+	
+	private String email;
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+
+}
